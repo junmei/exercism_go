a32cab9a842ad18e1859e5223c66d34ea8db2051
Junmei Duan <junmei.duan@pfsw.com>
2018-03-30 19:54:39 +0200

    Solve "leap"

---
+++ leap/leap.go
@@ -0,0 +1,12 @@
+// Package leap ...
+package leap
+
+const testVersion = 3
+
+// IsLeapYear Check if a year is a leap year.
+func IsLeapYear(year int) bool {
+	if year%4 == 0 && !(year%100 == 0 && year%400 != 0) {
+		return true
+	}
+	return false
+}
